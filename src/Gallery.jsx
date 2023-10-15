d3c5042fbe72cd7249a0fa46254460c10cf1bcae
Oskars Adamovičs <38877693+darkula@users.noreply.github.com>
2023-10-15 19:12:03 +0300

    Update Gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,8 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useGlobalContext } from "./context";
 
-console.log(import.meta.env.VITE_API_KEY);
-
 const url = `https://api.unsplash.com/search/photos?client_id=${
   import.meta.env.VITE_API_KEY
 }`;
